6e2a75cca26e8ab7c725206af5bf17883bda8273
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-11-15 23:43:42 -0500

    Update main_OG.js

--- scripts/main_OG.js
+++ scripts/main_OG.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let markers = [];
 
     // Load hospital data from JSON file
-    fetch('data/2025/2025_GW_HospitalScores.json')
+    fetch('data/2025/2025_Lown_Index_GA.json')
         .then(response => response.json())
         .then(data => {
             hospitalsData = data;
@@ -497,3 +497,4 @@ function applyMetricFilters(metrics) {
             });
     }
 });
+
